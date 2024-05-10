c745560e6fd437553bb367e14ecb8b3c4df1a87d
Set Maw Htay <setmawhtay@gmail.com>
2024-05-10 20:47:39 +0700

    first commit

---
+++ data/user-data.js
@@ -0,0 +1,13 @@
+export const userData = {
+  githubUser: 'setmawhtay',
+  devUsername: "setmawhtay",
+  github: 'https://github.com/setmawhtay',
+  facebook: 'https://www.facebook.com/setmawhtay/',
+  linkedIn: 'https://www.linkedin.com/in/setmawhtay/',
+  twitter: 'https://twitter.com/real_setmawhtay',
+  // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  // leetcode: "https://leetcode.com/said3812/",
+  resume: "your-resume-file-link",
+  skills: ['Linux', 'PHP', 'VueJS', 'CodeIgniter', 'Laravel', 'MySql', 'MongoDB', 'Postgres', 'Docker', 'AWS'],
+  timezone: '+7'
+};
